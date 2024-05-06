81651b54e1ff8e514b3be373884008db5ba744f4
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-05-06 15:10:27 +0530

    Update telegram_status.py

--- bot/helper/mirror_utils/status_utils/telegram_status.py
+++ bot/helper/mirror_utils/status_utils/telegram_status.py
@@ -5,7 +5,7 @@ from bot.helper.ext_utils.bot_utils import (MirrorStatus,
                                             get_readable_file_size,
                                             get_readable_time)
 
-engine_ = f"pyrogram v{get_distribution('pyrofork').version}"
+engine_ = f"pyrogram v{get_distribution('pyrogram').version}"
 
 class TelegramStatus:
     def __init__(self, obj, size, message, gid, status, extra_details):
